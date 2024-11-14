fecdfd0cae1890cc75a8a3328146002df21abb8f
Vinicius Dorini Giacomini <viniciusdgiacomini@gmail.com>
2024-11-14 20:30:12 -0300

    Lista 2

--- lista2_exercicio.py
+++ lista2_exercicio.py
@@ -3,14 +3,14 @@ num2 = input("Digite outro número: ")
 sinal = input("Digite o operador(+, -, *, /): ")
 
 if sinal == "+":
-    result = int(num1) + int(num2)
+    result = float(num1) + float(num2)
     print("Resultado: ", result)
 elif sinal == "-":
-    result = int(num1) - int(num2)
+    result = float(num1) - float(num2)
     print("Resultado: ", result)
 elif sinal == "*":
-    result = int(num1) * int(num2)
+    result = float(num1) * float(num2)
     print("Resultado: ", result)
 elif sinal == "/":
-    result = int(num1) / int(num2)
+    result = float(num1) / float(num2)
     print("Resultado: ", result)
